701d03ac0a0ecccc9539d303e26f47804b491e50
AQing-527 <aqing@connect.hku.hk>
2021-07-31 14:28:46 +0800

    add noise generator

--- code/circle_generator.py
+++ code/circle_generator.py
@@ -6,8 +6,9 @@ import time
 CANVAS_SIZE = (800, 800)
 SHAPE_COLOR = (255, 255, 255)
 # Customize shape name for easier identification
-SAVE_NAME = 'Circle_3'
-
+SAVE_NAME = 'Noise_Circle_1'
+AMPLITUDE = 20
+POINTS_NUM = 40
 
 class CircleDrawer(object):
     def __init__(self, window_name):
@@ -15,6 +16,7 @@ class CircleDrawer(object):
        self.x = -1
        self.y = -1
        self.radius = -1
+       self.ponits = []
 
     # (x,y) is the center of the circle
     def set_center(self, x, y):
@@ -25,14 +27,23 @@ class CircleDrawer(object):
         if (radius > 0):
             self.radius = radius
 
-    def run(self):
+    def run(self, noise):
          cv2.namedWindow(self.window_name)
          cv2.imshow(self.window_name, np.zeros(CANVAS_SIZE, np.uint8))
          canvas = np.zeros(CANVAS_SIZE, np.uint8)
          if (self.center[0] > 0 and self.center[1] > 0 and radius> 0):
-            # generate the circle using the input parameters
-             cv2.circle(canvas, self.center, self.radius, SHAPE_COLOR, thickness = 1)
-             cv2.imshow(self.window_name, canvas)
+             if (noise == False):
+                # generate the circle using the input parameters
+                cv2.circle(canvas, self.center, self.radius, SHAPE_COLOR, thickness = 1)
+                cv2.imshow(self.window_name, canvas)
+            
+             else:
+                for i in range(POINTS_NUM):
+                    self.points.append([self.x+(self.radius+AMPLITUDE)*np.cos((360/POINTS_NUM)*2*np.pi*i), self.y+(self.radius+AMPLITUDE)*np.sin((360/POINTS_NUM)*2*np.pi*i)])
+                    self.points.append([self.x+(self.radius+AMPLITUDE)*np.cos((360/POINTS_NUM)*2*np.pi*i+(180/POINTS_NUM)*2*np.pi), self.y+(self.radius+AMPLITUDE)*np.sin((360/POINTS_NUM)*2*np.pi*i(180/POINTS_NUM)*2*np.pi)])
+                cv2.polylines(canvas, pts=self.points, isClosed=True, color = SHAPE_COLOR, thickness=1)
+                cv2.imshow()
+            
          # Waiting for the user to press any key
          cv2.waitKey()     
          cv2.destroyWindow(self.window_name)
@@ -57,18 +68,31 @@ class DataSaver(object):
     def set_save_name(self, save_name):
         self.save_name = save_name
 
-    def save(self):
+    def save(self, noise):
         f = open(f'{self.data_path}{self.save_name}.txt', 'w')
-        # Normalize (x,y) and radius to (0,1)
-        x = self.x / CANVAS_SIZE[0]
-        y = self.y / CANVAS_SIZE[1]
-        # assume we always use square canvas
-        radius = self.radius / CANVAS_SIZE[0]
-        f.write(f'{x} {y} {radius}\n')
+        if not noise:
+            # Normalize (x,y) and radius to (0,1)
+            x = self.x / CANVAS_SIZE[0]
+            y = self.y / CANVAS_SIZE[1]
+            # assume we always use square canvas
+            radius = self.radius / CANVAS_SIZE[0]
+            f.write(f'{x} {y} {radius}\n')
 
-        f.close()
+            f.close()
 
-        cv2.imwrite(f'{self.image_path}{self.save_name}.png', self.image)
+            cv2.imwrite(f'{self.image_path}{self.save_name}.png', self.image)
+
+        else:
+            for point in self.points:
+                # Normalize (x,y) to (0,1)
+                x = np.double(np.double(point[0]) / np.double(CANVAS_SIZE[0]))
+                y = np.double(np.double(point[1]) / np.double(CANVAS_SIZE[1]))
+                f.write(f'{x} {y}\n')
+                
+                f.close()
+
+                cv2.imwrite(f'{self.image_path}{self.save_name}.png', self.image)
+
 
 if __name__ == '__main__':
     drawer = CircleDrawer('press any key to exit and store this circle')
@@ -80,12 +104,12 @@ if __name__ == '__main__':
     radius = int(input("Please enter the radius of this circle (the canvas size is 800*800): "))
     drawer.set_radius(radius)
 
-    image = drawer.run()
+    image = drawer.run(noise=False)
 
     saver = DataSaver(x, y, radius, image)
     if SAVE_NAME != '':
         saver.set_save_name(SAVE_NAME)
-    saver.save()
+    saver.save(noise=False)
     print(f'Data path = {saver.data_path}{saver.save_name}.txt')
     print(f'Image path = {saver.image_path}{saver.save_name}.png')
     print("Done!")
